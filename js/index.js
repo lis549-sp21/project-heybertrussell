9584c71750b7ed7b3f9cbf77ef1128f967adf2a3
heybertrussell <59258621+heybertrussell@users.noreply.github.com>
2022-05-28 14:20:35 -0700

    adding more css & JS

--- js/index.js
+++ js/index.js
@@ -1,10 +1,18 @@
 function myFunction(imgs) {
-    // Get the expanded image
     var expandImg = document.getElementById("expandedImg");
-    // Get the image text
     var imgText = document.getElementById("imgtext");
-    // Use the same src in the expanded image as the image being clicked on from the grid
     expandImg.src = imgs.src;
-    // Show the container element (hidden with CSS)
     expandImg.parentElement.style.display = "block";
   }
+
+
+  $(document).ready(function(){
+    $("button").click(function(){
+      $("p").css({"background-color": "black", "color": "black"});
+      $("li").css({"background-color": "black", "color": "black"});
+      $("h1").css({"background-color": "black", "color": "black"});
+      $("h2").css({"background-color": "black", "color": "black"});
+      $("button").css({"background-color": "black", "color": "black"});
+      $("nav").before('<div class="cops">Nothing to see here, officer<br><br><span style="font-size:100px">&#128519;</span></div>');
+  });
+  });
