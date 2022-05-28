dd055c4e640aa6f1e5c855d287d6ec19423d2627
heybertrussell <59258621+heybertrussell@users.noreply.github.com>
2022-05-27 22:58:15 -0700

    testing out some hidden stuff

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(window).load(function () {
+    $('.skillz').click(function(){
+       $('.hover_bkgr_fricc').show();
+    });
+    $('.hover_bkgr_fricc').click(function(){
+        $('.hover_bkgr_fricc').hide();
+    });
+    $('.popupCloseButton').click(function(){
+        $('.hover_bkgr_fricc').hide();
+    });
+});
